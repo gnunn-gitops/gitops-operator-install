555bafaaf241df5321b26a73efa6bd500cddad03
Gerald Nunn <gerald.b.nunn@gmail.com>
2021-05-03 17:50:59 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### Introduction
+
+This repository for bootstrapping the initial Red Hat gitops-operator into a cluster. The `setup.sh` performs the following steps:
+
+1. Install operator and wait for it to be complete
+2. Overwrite the default `openshift-gitops` instance with my preferred configuration.
+
+### Usage
+
+To install the operator, use the command:
+
+```
+./setup.sh <cluster-name>
+```
+
+Where `cluster-name` is the name of the cluster you want to install. This name must match an overlay in the `openshift-gitops/overlays` folder.
+
+The reason why we need overlay specific cluster configuration is that I prefer to use an SSO instance to manage the argocd authentication and this requires a cluster specific issuer URL, see the `home` overlay for an example.
---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+LANG=C
+SLEEP_SECONDS=45
+
+OVERLAY=default
+
+if [ $# -gt 0 ]; then
+    OVERLAY=$1
+    echo "Using overlay $1"
+fi
+
+exit 0;
+
+echo ""
+echo "Installing GitOps Operator."
+
+oc apply -k https://github.com/redhat-canada-gitops/catalog/openshift-gitops-operator/overlays/stable-4.7
+
+echo "Pause $SLEEP_SECONDS seconds for the creation of the gitops-operator..."
+sleep $SLEEP_SECONDS
+
+echo "Waiting for openshift-gitops namespace to be created"
+until oc get ns openshift-gitops
+do
+  sleep 5;
+done
+
+oc apply -k ./openshift-gitops
+
+echo "Waiting for all pods to be created"
+deployments (cluster kam openshift-gitops-applicationset-controller openshift-gitops-redis openshift-gitops-repo-server openshift-gitops-server)
+for i in "${my_array[@]}";
+do
+  echo "Waiting for deployment $i";
+  oc rollout status deployment $i
+done
+
+echo "Apply overlay to override default instance"
+kustomize build | oc apply -f -
+
+sleep 10
+echo "Waiting for all pods to redeploy"
+deployments (cluster kam openshift-gitops-applicationset-controller openshift-gitops-redis openshift-gitops-repo-server openshift-gitops-server)
+for i in "${my_array[@]}";
+do
+  echo "Waiting for deployment $i";
+  oc rollout status deployment $i
+done
+
+echo "GitOps Operator ready"
